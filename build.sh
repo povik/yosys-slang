bba1cc36b0c8c5fc40e54162fd881b7fc4928db4
Martin Povišer <povik@cutebit.org>
2024-06-11 18:11:31 +0200

    Fix boost-related build flags

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 cmake -S slang -B build/slang -DCMAKE_INSTALL_PREFIX=build/slang_install -DSLANG_USE_MIMALLOC=OFF \
 	-DSLANG_INCLUDE_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DSLANG_INCLUDE_TOOLS=OFF \
-	-DCMAKE_CXX_FLAGS="-fPIC"
+	-DCMAKE_CXX_FLAGS="-fPIC" -DBoost_NO_BOOST_CMAKE=ON
 make -C build/slang -j$(nproc)
 make -C build/slang install
 mkdir -p $(dirname "$TARGET")
