e0fa99a81a17a1f40b877b02352d0e382b4b70e0
Martin Povišer <povik@cutebit.org>
2024-07-23 10:31:59 +0200

    Move diagnostics externally

--- slang_frontend.cc
+++ slang_frontend.cc
@@ -1306,62 +1306,7 @@ static RTLIL::SigSpec evaluate_function(SignalEvalContext &eval, const ast::Call
 }
 #endif
 
-namespace diag {
-	slang::DiagCode IffUnsupported(slang::DiagSubsystem::Netlist, 1000);
-	slang::DiagCode SignalSensitivityAmbiguous(slang::DiagSubsystem::Netlist, 1001);
-	slang::DiagCode EdgeImplicitMixing(slang::DiagSubsystem::Netlist, 1002);
-	slang::DiagCode GenericTimingUnsyn(slang::DiagSubsystem::Netlist, 1003);
-	slang::DiagCode BothEdgesUnsupported(slang::DiagSubsystem::Netlist, 1004);
-	slang::DiagCode NoteSignalEvent(slang::DiagSubsystem::Netlist, 1005);
-	slang::DiagCode ExpectingIfElseAload(slang::DiagSubsystem::Netlist, 1006);
-	slang::DiagCode NoteDuplicateEdgeSense(slang::DiagSubsystem::Netlist, 1007);
-	slang::DiagCode IfElseAloadPolarity(slang::DiagSubsystem::Netlist, 1008);
-	slang::DiagCode IfElseAloadMismatch(slang::DiagSubsystem::Netlist, 1009);
-	slang::DiagCode LatchNotInferred(slang::DiagSubsystem::Netlist, 1010);
-	slang::DiagCode LatchInferred(slang::DiagSubsystem::Netlist, 1011);
-	slang::DiagCode MissingAload(slang::DiagSubsystem::Netlist, 1012);
-	slang::DiagCode NoteProcessDriver(slang::DiagSubsystem::Netlist, 1013);
-	slang::DiagCode AlwaysFFBadTiming(slang::DiagSubsystem::Netlist, 1014);
-
-	slang::DiagGroup unsynthesizable("unsynthesizable", {IffUnsupported, SignalSensitivityAmbiguous, GenericTimingUnsyn, BothEdgesUnsupported, ExpectingIfElseAload,
-														 IfElseAloadPolarity, IfElseAloadMismatch});
-	slang::DiagGroup sanity("sanity", {EdgeImplicitMixing});
-
-	void setup_messages(slang::DiagnosticEngine &engine)
-	{
-		engine.setMessage(IffUnsupported, "iff qualifier will not be synthesized");
-		engine.setMessage(SignalSensitivityAmbiguous, "non-edge sensitivity on a signal will be synthesized as @* sensitivity");
-		engine.setMessage(EdgeImplicitMixing, "mixing of implicit and edge sensitivity");
-		engine.setMessage(GenericTimingUnsyn, "unsynthesizable timing control");
-		engine.setMessage(BothEdgesUnsupported, "'edge' sensitivity will not be synthesized");
-		engine.setMessage(NoteSignalEvent, "signal event specified here");
-		engine.setSeverity(NoteSignalEvent, slang::DiagnosticSeverity::Note);
-
-		engine.setMessage(ExpectingIfElseAload, "a simple if-else pattern is expected in modeling an asynchronous load on a flip-flop");
-		engine.setMessage(NoteDuplicateEdgeSense, "asynchronous load pattern implied by edge sensitivity on multiple signals");
-		engine.setSeverity(NoteDuplicateEdgeSense, slang::DiagnosticSeverity::Note);
-
-		engine.setMessage(IfElseAloadPolarity, "polarity of the condition doesn't match the edge sensitivity");
-		engine.setMessage(IfElseAloadMismatch, "condition cannot be matched to any signal from the event list");
-		engine.setSeverity(LatchInferred, slang::DiagnosticSeverity::Error);
-
-		engine.setMessage(LatchNotInferred, "latch not inferred for signal '{}' driven from an always_latch process");
-		engine.setSeverity(LatchNotInferred, slang::DiagnosticSeverity::Error);
-		engine.setMessage(LatchInferred, "latch inferred for signal '{}' driven from an always_comb process");
-		engine.setSeverity(LatchInferred, slang::DiagnosticSeverity::Error);
-
-		engine.setMessage(MissingAload, "asynchronous load value missing for driven signal '{}'");
-		engine.setSeverity(MissingAload, slang::DiagnosticSeverity::Error);
-		engine.setMessage(NoteProcessDriver, "signal driven from this process");
-		engine.setSeverity(NoteProcessDriver, slang::DiagnosticSeverity::Note);
-
-		engine.setMessage(AlwaysFFBadTiming, "timing control does not model a flip-flop");
-		engine.setSeverity(AlwaysFFBadTiming, slang::DiagnosticSeverity::Error);
-
-		engine.setSeverity(unsynthesizable, slang::DiagnosticSeverity::Error);
-		engine.setSeverity(sanity, slang::DiagnosticSeverity::Error);
-	}
-};
+#include "diag.h"
 
 struct PopulateNetlist : public ast::ASTVisitor<PopulateNetlist, true, false> {
 public:
