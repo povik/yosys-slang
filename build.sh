e7d160046fca2097ea33c6f98489399d0be88cd7
Martin Povišer <povik@cutebit.org>
2024-09-01 16:19:38 +0200

    Fix sv-tests build

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -e
 # this file is here for the benefit of chipsalliance/sv-tests and it will
 # be retired once the build instructions within sv-tests are updated
 make -j$(nproc) YOSYS_PREFIX=${YOSYS_PREFIX}
+mkdir -p $(dirname "$TARGET")
 cp build/slang.so "$TARGET"
