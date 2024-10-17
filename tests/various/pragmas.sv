dfe0b708635c4785de6c44b4c1157bbccb22e703
Martin Povišer <povik@cutebit.org>
2024-10-17 14:02:17 +0200

    Add tests for translate_on/off pragmas

---
+++ tests/various/pragmas.sv
@@ -0,0 +1,15 @@
+module top;
+// pragma translate_off
+initial $error("1");
+// pragma translate_on
+
+// pragma synthesis_off
+initial $error("2");
+// pragma synthesis_on
+
+// pragma synthesis_off
+// this one should not get picked up: pragma synthesis_on
+// this one neither: pragma synthesis_onff
+initial $error("3");
+// pragma synthesis_on
+endmodule
