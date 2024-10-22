773b9fd75f2b8b87a9b7fa7dba6a1b800c86d44c
Martin Povišer <povik@cutebit.org>
2024-10-22 18:50:02 +0200

    Fix intent in pragmas test

--- tests/various/pragmas.sv
+++ tests/various/pragmas.sv
@@ -3,13 +3,14 @@ module top;
 initial $error("1");
 // pragma translate_on
 
-// pragma synthesis_off
+//pragma synthesis_off
 initial $error("2");
 // pragma synthesis_on
 
 // pragma synthesis_off
 // this one should not get picked up: pragma synthesis_on
-// this one neither: pragma synthesis_onff
+// this one neither:
+//pragma synthesis_onff
 initial $error("3");
 // pragma synthesis_on
 endmodule
