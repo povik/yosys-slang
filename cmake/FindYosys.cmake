719cddde9f4cd4923c21de7e16ea70f632b00895
Miodrag Milanovic <mmicko@gmail.com>
2025-04-03 08:10:40 +0200

    Fix build for Windows mingw environment

--- cmake/FindYosys.cmake
+++ cmake/FindYosys.cmake
@@ -22,4 +22,20 @@ message(STATUS "yosys-config --cxxflags (filtered): ${YOSYS_CXXFLAGS}")
 add_library(yosys::yosys INTERFACE IMPORTED)
 target_compile_options(yosys::yosys INTERFACE ${YOSYS_CXXFLAGS})
 
+if(WIN32)
+    execute_process(
+        COMMAND ${YOSYS_CONFIG} --linkflags
+        OUTPUT_VARIABLE YOSYS_LINKFLAGS
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+    string(REGEX REPLACE " +" ";" YOSYS_LINKFLAGS ${YOSYS_LINKFLAGS})
+    list(FILTER YOSYS_LINKFLAGS INCLUDE REGEX "^-[L]")
+    message(STATUS "yosys-config --linkflags (filtered): ${YOSYS_LINKFLAGS}")
+
+    target_link_options(yosys::yosys INTERFACE ${YOSYS_LINKFLAGS})
+
+    target_link_libraries(yosys::yosys INTERFACE yosys_exe)
+endif()
+
 set(YOSYS_DATDIR ${YOSYS_DATDIR} PARENT_SCOPE)
