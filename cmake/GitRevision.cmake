c041128b4ee5a32a7703191011cb72c0f3a7325c
Catherine <whitequark@whitequark.org>
2025-06-30 15:15:03 +0000

    Fill in default values for git revision if unavailable.
    Fixes #141.

--- cmake/GitRevision.cmake
+++ cmake/GitRevision.cmake
@@ -1,12 +1,20 @@
 function(git_rev_parse output_var source_dir)
 
     if (NOT DEFINED ${output_var})
+        set(command git -C ${source_dir} rev-parse HEAD)
+
         execute_process(
-            COMMAND git -C ${source_dir} rev-parse HEAD
+            COMMAND ${command}
             OUTPUT_VARIABLE ${output_var}
             OUTPUT_STRIP_TRAILING_WHITESPACE
-            COMMAND_ERROR_IS_FATAL ANY
+            RESULT_VARIABLE result
         )
+
+        if (NOT result EQUAL 0)
+            list(JOIN command " " command)
+            message(WARNING "Command \"${command}\" failed; VCS information will be unavailable")
+            set (${output_var} "UNKNOWN")
+        endif()
     endif()
 
     message(STATUS "Got ${output_var}: ${${output_var}}")
